26e4993b36635f04e1cfb9f327f74391eee6df4b
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-10-03 16:50:24 +0200

    FavoritesPage fixed

---
+++ src/components/pages/FavoritesPage/FavoritesPage.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useContext } from 'react'
+import AppContext from 'provider/AppContext'
+import uuid from 'react-uuid'
+import './FavoritesPage.scss'
+
+import Box from 'components/global/Box'
+import Loader from 'components/ui/Loader'
+
+/**
+ * Favorites page component
+ * @return {object} component with children
+ */
+const Favorites = () => {
+	const { isLoading, favorites } = useContext(AppContext)
+
+	return (
+		<Fragment>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<div className="main__section--boxes">
+					{favorites.map(item => (
+						<Box key={uuid()} item={item} />
+					))}
+				</div>
+			)}
+		</Fragment>
+	)
+}
+
+/**
+ * Display name
+ * @type {string}
+ */
+Favorites.displayName = 'Favorites Page'
+
+export default Favorites
